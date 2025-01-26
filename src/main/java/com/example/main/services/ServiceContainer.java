db87ecda097e3b5867f0c0154416b8d56a56f13d
giulianoh92 <giulianohillebrand@gmail.com>
2025-01-26 15:50:16 -0300

    Add OrdersService and ProductsService to ServiceContainer with repository integrations

--- src/main/java/com/example/main/services/ServiceContainer.java
+++ src/main/java/com/example/main/services/ServiceContainer.java
@@ -3,10 +3,15 @@ package com.example.main.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.example.main.services.Orders.OrdersService;
+import com.example.main.services.Products.ProductsService;
 import com.example.main.services.Users.UserService;
-
+import com.example.main.domain.repositories.CartsRepository;
+import com.example.main.domain.repositories.CategoriesRepository;
+import com.example.main.domain.repositories.ItemsRepository;
 import com.example.main.domain.repositories.OrdersRepository;
 import com.example.main.domain.repositories.ProductsRepository;
+import com.example.main.domain.repositories.StatusesRepository;
 import com.example.main.domain.repositories.UsersRepository;
 
 @Component
@@ -20,8 +25,26 @@ public class ServiceContainer {
 
     @Autowired
     private ProductsRepository productsRepository;
+    
+    @Autowired
+    private CartsRepository cartsRepository;
 
     @Autowired
-    public UserService userService = new UserService(usersRepository);
+    private ItemsRepository itemsRepository;
+
+    @Autowired
+    private CategoriesRepository categoriesRepository;
+
+    @Autowired 
+    private StatusesRepository statusesRepository;
+
+    @Autowired
+    public UserService userService = new UserService(usersRepository, cartsRepository);
+
+    @Autowired
+    public OrdersService ordersService = new OrdersService(ordersRepository, itemsRepository, statusesRepository);
+
+    @Autowired
+    public ProductsService productsService = new ProductsService(productsRepository, categoriesRepository);
 
 }
