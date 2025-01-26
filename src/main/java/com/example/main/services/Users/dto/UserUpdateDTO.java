ec18e07077e9d9cdf8657dfb740561658ac98a0e
giulianoh92 <giulianohillebrand@gmail.com>
2025-01-26 15:47:48 -0300

    Remove unused validation annotations from UserUpdateDTO

--- src/main/java/com/example/main/services/Users/dto/UserUpdateDTO.java
+++ src/main/java/com/example/main/services/Users/dto/UserUpdateDTO.java
@@ -1,8 +1,4 @@
 package com.example.main.services.Users.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
